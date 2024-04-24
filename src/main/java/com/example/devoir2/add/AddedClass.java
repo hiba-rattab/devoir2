d42c417101a34686c06aca218902f5edfe1b56a2
Saloua_jbabri <salouajb.44@gmail.com>
2024-04-24 11:21:02 +0200

    i added a file

---
+++ src/main/java/com/example/devoir2/add/AddedClass.java
@@ -0,0 +1,17 @@
+package com.example.devoir2.add;
+
+public class AddedClass {
+private int num;
+
+    public AddedClass(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
