df88b876f5a0c783112e622a083da0a8dfc47299
unknown <rattabhiba@gmail.com>
2024-04-24 09:28:18 +0100

    project

---
+++ src/main/java/com/example/devoir2/Devoir2Application.java
@@ -0,0 +1,13 @@
+package com.example.devoir2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devoir2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devoir2Application.class, args);
+    }
+
+}
